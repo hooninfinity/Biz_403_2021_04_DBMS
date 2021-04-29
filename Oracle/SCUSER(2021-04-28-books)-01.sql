b521a6fc32c6f379167f3037825635f1632be3e9
hooninfinity <hooninfinity@naver.com>
2021-04-29 12:50:10 +0900

    JOIN

--- Oracle/SCUSER(2021-04-28-books)-01.sql
+++ Oracle/SCUSER(2021-04-28-books)-01.sql
@@ -180,25 +180,3 @@ VALUES( 37, '9791162540572', '홍길동');
 
 
 
-
-
-
-
-
-
-
---INSERT ALL
---INTO tbl_books(bk_isbn,bk_publisher,bk_bname,bk_writer,bk_trans,bk_date,bk_page,bk_price) VALUES(9791162540770,'비즈니스북스','데스 바이 아마존','시로타 마코토','신희원','2019-04-15',272,15000)
---INTO tbl_books(bk_isbn,bk_publisher,bk_bname,bk_writer,bk_trans,bk_date,bk_page,bk_price) VALUES(9791188850549,'북라이프','4주만에 완성하는 레깅스핏 스트레칭','모리 다쿠로','신희원','2019-04-15',272,15000)
---INTO tbl_books(bk_isbn,bk_publisher,bk_bname,bk_writer,bk_date,bk_page,bk_price) VALUES(9791177750518,'북라이프','왕이 된 남자 2','김선덕','2019-04-15',272,15000)
---INTO tbl_books(bk_isbn,bk_publisher,bk_bname,bk_writer,bk_date,bk_page,bk_price) VALUES(9791188850501,'북라이프','왕이 된 남자 1','김선덕','2019-04-15',272,15000)
---INTO tbl_books(bk_isbn,bk_publisher,bk_bname,bk_writer,bk_date,bk_page,bk_price) VALUES(9791162540756,'비즈니스북스','새벽에 읽는 유대인 인생 특강','장대은','2019-04-15',272,15000)
---INTO tbl_books(bk_isbn,bk_publisher,bk_bname,bk_writer,bk_date,bk_page,bk_price) VALUES(9791188850471,'북라이프','왕이 된 남자 포토에세이','스튜디오 드래곤','2019-04-15',272,15000)
---INTO tbl_books(bk_isbn,bk_publisher,bk_bname,bk_writer,bk_trans,bk_date,bk_page,bk_price) VALUES(9791162540732,'비즈니스북스','오토노미 제2의 이동 혁명','로렌스 번스 - 크리스토퍼 슐건','김현정','2019-04-15',272,15000)
---INTO tbl_books(bk_isbn,bk_publisher,bk_bname,bk_writer,bk_trans,bk_date,bk_page,bk_price) VALUES(9791162540718,'비즈니스북스','쓴다 쓴다 쓰는대로 된다','후루카와 다케시','유나현','2019-04-15',272,15000)
---INTO tbl_books(bk_isbn,bk_publisher,bk_bname,bk_writer,bk_date,bk_page,bk_price) VALUES(9791162540695,'비즈니스북스','머니패턴','이요셉 - 김채송화','2019-04-15',272,15000)
---INTO tbl_books(bk_isbn,bk_publisher,bk_bname,bk_writer,bk_date,bk_page,bk_price) VALUES(9791162540671,'비즈니스북스','1日 1行의 기적','유근용','2019-04-15',272,15000)
---INTO tbl_books(bk_isbn,bk_publisher,bk_bname,bk_writer,bk_date,bk_page,bk_price) VALUES(9791188850440,'북라이프','오늘도 울컥하고 말았습니다','정민지','2019-04-15',272,15000)
---INTO tbl_books(bk_isbn,bk_publisher,bk_bname,bk_writer,bk_trans,bk_date,bk_page,bk_price) VALUES(9791188850426,'북라이프','엘리트 제국의 몰락','미하엘 하르트만','이덕임','2019-04-15',272,15000)
---INTO tbl_books(bk_isbn,bk_publisher,bk_bname,bk_writer,bk_trans,bk_date,bk_page,bk_price) VALUES(9791162540640,'비즈니스북스','아주 작은 습관의 힘','제임스 클리어','이한이','2019-04-15',272,15000)
---INTO tbl_books(bk_isbn,bk_publisher,bk_bname,bk_writer,bk_trans,bk_date,bk_page,bk_price) VALUES(9791186805398,'비즈니스북스','그릿 GRIT(100쇄 기념 리커버 에디션)','앤절라 더크워스','김미정','2019-04-15',272,15000)
