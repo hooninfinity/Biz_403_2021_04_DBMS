ba95cb3b90c471188846ae62513794d95bbc083e
hooninfinity <hooninfinity@naver.com>
2021-05-27 16:32:32 +0900

    todo

--- TODOLIST/TODOLIST(2021-05-26).sql
+++ TODOLIST/TODOLIST(2021-05-26).sql
@@ -4,7 +4,7 @@ CREATE TABLE tbl_todolist (
 	td_seq BIGINT auto_increment PRIMARY KEY,
     td_sdate VARCHAR(10) NOT NULL, -- 추가된 날짜
     td_stime VARCHAR(10) NOT NULL, -- 추가된 시간
-    td_doit VARCHAR(10) NOT NULL,
+    td_doit VARCHAR(300) NOT NULL,
     
     td_edate VARCHAR(10) DEFAULT '', -- 완료된 날짜
     td_etime VARCHAR(10) DEFAULT '' -- 완료 시간
