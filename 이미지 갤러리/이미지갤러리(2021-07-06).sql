605b1dfa828e6890f3bb7a0d883c11535b2af38e
hooninfinity <hooninfinity@naver.com>
2021-07-06 14:08:19 +0900

    gallery

---
+++ 이미지 갤러리/이미지갤러리(2021-07-06).sql
@@ -0,0 +1,22 @@
+USE myLibs;
+
+CREATE TABLE tbl_gallery (
+	g_seq	BIGINT	AUTO_INCREMENT	PRIMARY KEY,
+	g_writer	VARCHAR(20)	NOT NULL,	
+	g_date	VARCHAR(10)	NOT NULL,	
+	g_time	VARCHAR(10)	NOT NULL,	
+	g_subject	VARCHAR(50)	NOT NULL,	
+	g_content	VARCHAR(1000)	NOT NULL,	
+	g_image	VARCHAR(125)		
+);
+SHOW TABLES;
+DROP TABLE tbl_gallery;
+
+CREATE TABLE tbl_files (
+	file_seq	BIGINT	AUTO_INCREMENT	PRIMARY KEY,
+	file_gseq	BIGINT	NOT NULL,	
+	file_original	VARCHAR(125)	NOT NULL,
+	file_upname	VARCHAR(125)	NOT NULL	
+);
+SHOW TABLES;
+DROP TABLE tbl_files;
